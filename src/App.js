2b805f7b7dd74373aaee1a3271efdb152110e9f5
EV1L46rus <vislik46rus@gmail.com>
2023-02-24 23:03:46 +0300

    hooks

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Hover from './components/Hover';
+import List from './components/List';
+import useInput from './hooks/useInput';
+
+function App() {
+
+  return (
+    <div className="App">
+      <List/>
+    </div>
+  );
+}
+
+export default App;
